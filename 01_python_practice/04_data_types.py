8d82d55a2b28fb35dcb6cbcfdf7ad93e9b2c25fc
GouravGC <chhatwanigourav@gmail.com>
2025-12-04 23:13:07 +0530

    Update

---
+++ 01_python_practice/04_data_types.py
@@ -0,0 +1,8 @@
+x = 5 # integer
+y = 5.0 # float
+z = 'Jai Shree Ram' # string
+a =[1,2,3] # list of integers
+a_1 = ['Gourav','Ram','Shyam'] # list of strings
+b= (1,2,3) # tuple 
+c = {'Gourav':25,'Ram':30,'Shyam':28} # dictionary
+d = {1,2,3} # set

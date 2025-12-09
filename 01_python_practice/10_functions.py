a0408c5f6fe8c695128f2fa464418908b8c56038
GouravGC <chhatwanigourav@gmail.com>
2025-12-09 11:01:32 +0530

    Updated

--- 01_python_practice/10_functions.py
+++ 01_python_practice/10_functions.py
@@ -58,3 +58,8 @@ x = lambda a,b:a*b
 print(x(4,5))
 
 x(4,5)
+
+# # Map Function
+# numbers = [1, 2, 3, 4, 5]
+# squared_numbers = list(map(lambda x: x ** 2, numbers))
+# print(squared_numbers)  # Output: [1, 4, 9, 16, 25]

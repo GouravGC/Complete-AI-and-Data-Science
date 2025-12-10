d10f84c541c392959511908cdd15fc744563f64b
GouravGC <chhatwanigourav@gmail.com>
2025-12-10 13:28:14 +0530

    Updated

---
+++ 01_python_practice/11_modules.py
@@ -0,0 +1,14 @@
+pip install pandas
+pip install numpy pandas matplotlib seaborn scipy statsmodels
+
+
+import pandas # also works
+import pandas as pd # alias for pandas nickname
+from pandas import DataFrame, Series # specific imports
+import pandas.read_csv as read # importing via alias
+import math 
+from math import sqrt
+
+print(sqrt(9))
+print(math.sqrt(9))
+

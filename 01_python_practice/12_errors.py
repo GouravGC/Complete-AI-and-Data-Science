6c92f91baf99b3bc800c17118a1e1ab70aafa4a0
GouravGC <chhatwanigourav@gmail.com>
2025-12-11 13:14:43 +0530

    Updated

---
+++ 01_python_practice/12_errors.py
@@ -0,0 +1,29 @@
+# syntax or indentation errors
+x = 1
+if (x > 0):
+print("x is a positive number")
+
+# logical or human errors
+if x > 0:
+    print('y is postive number')
+
+# runtime errors
+# runtime or module not found errors
+import pandas as pd
+print(pd.sqrt(9))
+
+# miscellaneous errors
+
+# syntax errors
+# Name error
+# type error
+# index error
+# key error
+# value error
+# attribute error
+# zero division error (1/0)
+# File not found error
+# import error
+# memory error
+# timeout error
+
